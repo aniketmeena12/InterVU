245f7884ec9977695d2bf8cec55468c5475a18bc
aniket meena <meenaaniket066@gmail.com>
2025-07-28 22:18:52 +0530

    intial

--- src/components/tooglecontainer.tsx
+++ src/components/tooglecontainer.tsx
@@ -5,14 +5,12 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import { useAuth } from "@clerk/clerk-react"
 import { Menu } from "lucide-react"
 import NavigationContainer from "./navigationcontainer";
-import { NavLink } from "react-router-dom";
-import { cn } from "@/lib/utils";
+
 
 const ToogleContainer = () => {
-  const {userId} = useAuth();
+  
   return (
 <Sheet>
   <SheetTrigger className="block md:hidden"><Menu/></SheetTrigger>
@@ -22,16 +20,7 @@ const ToogleContainer = () => {
     </SheetHeader>
     <nav className=" p-4 py-2 gap-6 flex flex-col items-start">
         <NavigationContainer isMobile/>
-          {userId && (
-            <NavLink
-              to={"/generate"}
-              className={({ isActive }) =>
-                cn("text-base text-neutral-600", isActive && "text-neutral-900 font-semibold")
-              }
-            >
-              Take An Interview
-            </NavLink>
-          )}
+          
     </nav>
   </SheetContent>
 </Sheet>
